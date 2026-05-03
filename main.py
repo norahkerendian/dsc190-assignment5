f2fa882247143aef01adfee6885fb9ca8273b181
norahkerendian <norahkerendian@gmail.com>
2026-05-03 14:54:22 -0700

    Break ruff rules intentionally

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from dsc190-assignment5!")
+import os
+import datetime
 
+TODO
+def main():
+    x = 4
+    print(  "Hello from dsc190-assignment5!"  )
+    print( datetime.date.today())
+    return None
 
 if __name__ == "__main__":
     main()
