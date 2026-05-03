3196803ebd8d79634f6e60fca6923f7852ec71fb
norahkerendian <norahkerendian@gmail.com>
2026-05-03 15:11:50 -0700

    Fixed styling errors

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-import datetime
-
-TODO
 def main():
-    x = 4
-    print(  "Hello from dsc190-assignment5!"  )
-    print( datetime.date.today())
-    return None
+    print("Hello from dsc190-assignment5!")
 
 if __name__ == "__main__":
     main()
